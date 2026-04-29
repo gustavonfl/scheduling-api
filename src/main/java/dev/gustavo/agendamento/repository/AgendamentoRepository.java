f9ada1ae84228a89582c6a71e322accccf1dcfcb
Gustavo Leite <qustavoleite@gmail.com>
2026-04-29 16:41:01 -0300

    feat: adiciona Query para evitar conflitos

---
+++ src/main/java/dev/gustavo/agendamento/repository/AgendamentoRepository.java
@@ -0,0 +1,25 @@
+package dev.gustavo.agendamento.repository;
+
+import dev.gustavo.agendamento.model.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    @Query("""
+        SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END
+            FROM Agendamento a
+            WHERE a.usuario = :usuario
+            AND a.status <> : dev.gustavo.agendamento.model.StatusAgendamento.AGENDADO
+            AND (a.dataInicio < :fim AND a.dataFim > :inicio)
+            AND (:ignoreId IS NULL OR a.id <> :ignoreId)
+""")
+
+    boolean existsConflito(@Param("usuario") String usuario,
+                           @Param("inicio") LocalDateTime inicio,
+                           @Param("fim") LocalDateTime fim,
+                           @Param("id") Long ignoreId);
+}
